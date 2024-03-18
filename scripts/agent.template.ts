649e186333d2d33ff4d254deb751c3b6b852e129
Joris van de Donk <jorisvddonk@gmail.com>
2024-03-18 23:41:28 +0100

    Default agent implementation: Allow forcing the baseURL via environment variable

--- scripts/agent.template.ts
+++ scripts/agent.template.ts
@@ -1,34 +1,42 @@
-const response = await fetch("${req.protocol}://${req.get('host')}/tasks/get");
+let SHEETBOT_BASEURL;
+if (!Deno.env.has("SHEETBOT_BASEURL")) {
+  SHEETBOT_BASEURL
+  SHEETBOT_BASEURL = "${req.protocol}://${req.get('host')}";
+  Deno.env.set("SHEETBOT_BASEURL", SHEETBOT_BASEURL);
+} else {
+  SHEETBOT_BASEURL = Deno.env.get("SHEETBOT_BASEURL");
+}
+
+const response = await fetch(SHEETBOT_BASEURL + "/tasks/get");
 const json = await response.json();
 if (json.hasOwnProperty("script")) {
-  Deno.env.set("SHEETBOT_BASEURL", "${req.protocol}://${req.get('host')}");
   Deno.env.set("SHEETBOT_TASK_ID", json.id);
   Deno.env.set(
     "SHEETBOT_TASK_BASEURL",
-    "${req.protocol}://${req.get('host')}/tasks/" + json.id,
+    SHEETBOT_BASEURL + "/tasks/" + json.id,
   );
   Deno.env.set(
     "SHEETBOT_TASK_ACCEPTURL",
-    "${req.protocol}://${req.get('host')}/tasks/" + json.id + "/accept",
+    SHEETBOT_BASEURL + "/tasks/" + json.id + "/accept",
   );
   Deno.env.set(
     "SHEETBOT_TASK_COMPLETEURL",
-    "${req.protocol}://${req.get('host')}/tasks/" + json.id + "/complete",
+    SHEETBOT_BASEURL + "/tasks/" + json.id + "/complete",
   );
   Deno.env.set(
     "SHEETBOT_TASK_FAILEDURL",
-    "${req.protocol}://${req.get('host')}/tasks/" + json.id + "/failed",
+    SHEETBOT_BASEURL + "/tasks/" + json.id + "/failed",
   );
   Deno.env.set(
     "SHEETBOT_TASK_DATAURL",
-    "${req.protocol}://${req.get('host')}/tasks/" + json.id + "/data",
+    SHEETBOT_BASEURL + "/tasks/" + json.id + "/data",
   );
   Deno.env.set(
     "SHEETBOT_TASK_ARTEFACTURL",
-    "${req.protocol}://${req.get('host')}/tasks/" + json.id + "/artefacts",
+    SHEETBOT_BASEURL + "/tasks/" + json.id + "/artefacts",
   );
 
-  await fetch(Deno.env.get("SHEETBOT_TASK_ACCEPTURL"), {
+  await fetch(Deno.env.get("SHEETBOT_TASK_ACCEPTURL")!, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -37,7 +45,7 @@ if (json.hasOwnProperty("script")) {
   });
   try {
     const data = await import(json.script);
-    await fetch(Deno.env.get("SHEETBOT_TASK_COMPLETEURL"), {
+    await fetch(Deno.env.get("SHEETBOT_TASK_COMPLETEURL")!, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -46,7 +54,7 @@ if (json.hasOwnProperty("script")) {
     });
   } catch (e) {
     console.error(e);
-    await fetch(Deno.env.get("SHEETBOT_TASK_FAILEDURL"), {
+    await fetch(Deno.env.get("SHEETBOT_TASK_FAILEDURL")!, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
