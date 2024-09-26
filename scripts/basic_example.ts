c7aef57505e2c246345e9c12143b024f63dcbf77
Joris van de Donk <jorisvddonk@gmail.com>
2024-09-26 21:33:04 +0200

    add basic example that showcases some data logging

---
+++ scripts/basic_example.ts
@@ -0,0 +1,39 @@
+import $ from "https://deno.land/x/dax/mod.ts";
+import { submitData, getData } from "./lib/taskutil.ts";
+import { addSheetData } from "./lib/sheetutil.ts";
+
+/*
+Suggested capabilitiesSchema for this task: <capabilitiesSchema>
+
+{
+  "$schema": "http://json-schema.org/draft-07/schema#",
+  "type": "object",
+  "properties": {
+    "packages": {
+      "type": "array",
+      "allOf": [
+        {
+          "contains": {
+            "const": "git"
+          }
+        }
+      ]
+    }
+  },
+  "required": [
+    "packages"
+  ]
+}
+
+</capabilitiesSchema>*/
+
+async function setData(name, value) {
+  const data = {};
+  data[name] = value;
+  return submitData(data);
+}
+
+const taskData = await getData();
+await setData("hello", new Date().toISOString());
+const git_version = await $`git --version`.text();
+await setData("git version", git_version);
