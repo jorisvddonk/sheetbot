14863641bea2a3456c883d87be9f1aa227221265
Joris van de Donk <jorisvddonk@gmail.com>
2025-01-07 21:29:34 +0000

    godot-compile: only upload relevant artefacts, add branch checkout support, add additional compile flag support, add suggested data/name etc

--- scripts/godot-compile.ts
+++ scripts/godot-compile.ts
@@ -9,6 +9,19 @@ import { getTask } from "./lib/taskutil.ts";
 import { addSheetData } from "./lib/sheetutil.ts";
 
 /*
+Suggested name for this task: <name>Godot compile</name>
+
+AddTaskComments: <addTaskComments>
+If you want to build a release template, use "additional_build_flags": "target=template_release"
+</addTaskComments>
+
+Suggested data for this task: <data>
+{
+  "branch": "4.3-stable",
+  "additional_build_flags": ""
+}
+</data>
+
 Suggested capabilitiesSchema for this task: <capabilitiesSchema>
 
 {
@@ -81,39 +94,63 @@ Suggested capabilitiesSchema for this task: <capabilitiesSchema>
 
 </capabilitiesSchema>*/
 
+const taskdata = await getData();
+let sheet = taskdata.sheet !== undefined ? taskdata.sheet : 'godot';
+let branch = taskdata.branch !== undefined ? taskdata.branch : '4.3-stable';
+let additional_build_flags = taskdata.additional_build_flags !== undefined ? taskdata.additional_build_flags : '';
+
 async function subtask_statusupdate(subtaskname, completed) {
   const data = {};
   data["subtask/" + subtaskname] = completed ? true : false;
   return submitData(data);
 }
 
+await subtask_statusupdate("git clone", false);
+await subtask_statusupdate("git fetch", false);
+await subtask_statusupdate("git switch", false);
+await subtask_statusupdate("scons - build", false);
+await subtask_statusupdate("upload artefacts", false);
+await submitData({"commit hash": "n/a"});
+
 if (Deno.env.has("GODOT_DIR")) {
-  // we expect there to be a checkout already, so just pull!
+  // we expect there to be a checkout already, so just change directory to it!
   $.cd(Deno.env.get("GODOT_DIR"));
-  await subtask_statusupdate("git pull", false);
-  await $`git pull`;
-  await subtask_statusupdate("git pull", true);
 } else {
   // create a temporary directory and git clone there (shallowly)!
   let tmpdir = tmp.dirSync().name;
-  $.cd(tmpdir);
-  await subtask_statusupdate("git clone", false);
+  $.cd(tmpdir);  
   await $`git clone --depth 1 https://github.com/godotengine/godot .`;
   await subtask_statusupdate("git clone", true);
 }
 
-await subtask_statusupdate("scons - build", false);
-if (Deno.build.os === "windows") {
-  await $`scons platform=windows precision=double`;
+await $`git switch --orphan temp_orphan`;
+await $`git remote set-branches origin '${branch}'`;
+await $`git fetch --depth 1 origin '${branch}:${branch}'`;
+await subtask_statusupdate("git fetch", true);
+await $`git switch --discard-changes '${branch}'`;
+await subtask_statusupdate("git switch", true);
+
+let sheet_key = undefined;
+if (taskdata.sheet_key !== undefined) {
+    sheet_key = taskdata.sheet_key;
 } else {
-  await $`scons platform=linuxbsd precision=double use_llvm=yes`;
+    const git_commit_hash = await $`git rev-parse HEAD`.text();
+    await submitData({"commit hash": git_commit_hash});
+    sheet_key = git_commit_hash.trim();
+}
+console.log("Sheet_key is " + sheet_key + ", sheet is " + sheet);
+
+if (Deno.build.os === "windows") {
+  await $`scons platform=windows precision=double ${additional_build_flags}`;
+} else {
+  await $`scons platform=linuxbsd precision=double use_llvm=yes ${additional_build_flags}`;
 }
 await subtask_statusupdate("scons - build", true);
 
 // done! Upload the artefact(s)!
 
 let foundArtefacts: Array<string> = [];
-for await (const e of walk('./bin', { match: [/godot\.\S+?\.editor\.double\.\S+/gi] })) {
+for await (const e of walk('./bin', { match: [/godot\.\S+?\.(editor|template_release)(\.double)?.\S+(exe|llvm|x86_64)$/gi] })) {
     if (e.isFile) {
         const foundArtefact = e.path;
         const tmpobj = tmp.fileSync({ mode: 0o644, prefix: basename(foundArtefact), postfix: '.tar.gz', discardDescriptor: true }).name.replaceAll("\\", "/");
@@ -125,19 +162,6 @@ for await (const e of walk('./bin', { match: [/godot\.\S+?\.editor\.double\.\S+/
         console.log(artefactdata.directURL);
         await Deno.remove(tmpobj);
 
-        const taskdata = await getData();
-
-        let sheet_key = undefined;
-        if (taskdata.sheet_key !== undefined) {
-            sheet_key = taskdata.sheet_key;
-        } else {
-            const git_commit_hash = await $`git rev-parse HEAD`.text();
-            sheet_key = git_commit_hash.trim();
-        }
-        let sheet = taskdata.sheet !== undefined ? taskdata.sheet : 'godot';
-        
-        console.log("Sheet_key is " + sheet_key + ", sheet is " + sheet);
-
         await addSheetData(sheet, {
             key: sheet_key,
             [tgz_filename]: artefactdata.directURL
@@ -146,3 +170,4 @@ for await (const e of walk('./bin', { match: [/godot\.\S+?\.editor\.double\.\S+/
         console.log("Done! with artefact ", foundArtefact.path);
     }
 }
+await subtask_statusupdate("upload artefacts", true);
