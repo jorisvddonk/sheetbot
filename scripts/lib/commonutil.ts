1d7578018f3dc1dc1221ade2edd47b7f0d01698d
Joris van de Donk <jorisvddonk@gmail.com>
2025-11-06 20:19:57 +0100

    fix deno 2

--- scripts/lib/commonutil.ts
+++ scripts/lib/commonutil.ts
@@ -1,4 +1,7 @@
 export function checkError(res) {
+    if (!res || !res.status) {
+        throw new Error("Invalid response object");
+    }
     if (res.status.toString()[0] !== '2') {
         throw new Error("Request response not OK: " + res.status);
     }
