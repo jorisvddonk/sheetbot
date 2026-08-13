b3d1a57a78cca5ac27b49996fc4fad2a664dbfc0
Joris van de Donk <jorisvddonk@gmail.com>
2026-08-13 14:42:15 +0200

    fix sticky headers (own scroll container) + fix pagination gotoPage wrapping rows into data key

--- static/element-grid.js
+++ static/element-grid.js
@@ -253,7 +253,7 @@ export class GridElement extends LitElement {
     }
 
     tableGenerator(tabledef) {
-        return html`
+        const grid = html`
             <div class="grid${this.stickyHeaders ? " sticky-headers" : ""}">
                 ${tabledef.columns.map((column, columnindex) => {
                     return html`<div class="cell header" style="grid-column: ${columnindex + 1}; grid-row: 1" col="${columnindex}" @click="${event => this.selectColumn(columnindex, event)}">
@@ -335,6 +335,10 @@ export class GridElement extends LitElement {
                 })}
             </div>
         `;
+        if (this.stickyHeaders) {
+            return html`<div class="grid-scroll">${grid}</div>`;
+        }
+        return grid;
     }
 
     /**
